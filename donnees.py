f96de88c7fba19a6c8ae6dc9cc1172d537517eef
Juliette Boyer <juliette.boyer@etu.minesparis.psl.eu>
2026-06-29 10:46:16 +0200

    Generer points aléatoires

--- donnees.py
+++ donnees.py
@@ -26,3 +26,32 @@ infos, demande = lire_scenario("mines_tms_instances/A_D_E_0.txt")
 
 print(infos)
 print(demande)
+
+#Générer graphe
+
+import random
+
+def generer_coordonnees(nb_clients, delta=20):
+    """
+    Génère les coordonnées des clients.
+    Le dépôt est en (0,0)
+    Paramètres :
+        nb_clients : nombre de clients
+        delta : les coordonnées sont tirées entre -delta et +delta
+    Retour :
+        liste des coordonnées [(0,0), (x1,y1), ..., (xn,yn)]
+    """
+    coordonnees = [(0, 0)]  # dépôt
+
+    for _ in range(nb_clients):
+        x = random.randint(-delta, delta)
+        y = random.randint(-delta, delta)
+        coordonnees.append((x, y))
+
+    return coordonnees
+
+#Test
+infos, demande = lire_scenario("mines_tms_instances/A_D_E_4.txt")
+nb_clients = infos[0]
+coordonnees = generer_coordonnees(nb_clients)
+print(coordonnees)
