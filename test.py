bb4d8136ef39637a352f9cc24d4f0ca5aa5cbb62
Juliette Boyer <juliette.boyer@etu.minesparis.psl.eu>
2026-06-30 11:17:22 +0200

    main

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from space_subdivision import space_subdiv
 
 
 
-infos, demande = lire_scenario("mines_tms_instances/C_S_H_0.txt")
+infos, demande = lire_scenario("mines_tms_instances/A_D_E_0.txt")
 nb_client = infos[0]
 li_client = generer_coordonnees(nb_client, delta=20 )[1:]
 P_max_camion = infos[-1]
@@ -12,7 +12,7 @@ li_poids = demande[0]
 print(space_subdiv(li_client, P_max_camion, li_poids))
 
 #affichage
-import matplotlib.pyplot as plt
+'''import matplotlib.pyplot as plt
 
 def afficher_zones(zones):
     plt.figure(figsize=(8, 8))
@@ -44,4 +44,4 @@ def afficher_zones(zones):
     plt.show()
 
 zones = space_subdiv(li_client, P_max_camion, li_poids)
-print(afficher_zones(zones))
+print(afficher_zones(zones))'''
